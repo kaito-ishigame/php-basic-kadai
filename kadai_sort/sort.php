43a0cc492bc82bac0ef6f48f862103acfe0235a5
kaito-ishigame <kuiren24@gmail.com>
2026-01-07 16:11:05 +0900

    修正

--- kadai_sort/sort.php
+++ kadai_sort/sort.php
@@ -9,35 +9,25 @@
 <body>
     <p>
         <?php
-        // 元の配列
+        // 配列
         $nums = [15, 4, 18, 23, 10];
 
-        // sort_2way関数
-        function sort_2way(&$array, $order) {
-            if ($order === 'asc') {
-                sort($array, SORT_NUMERIC);
-            } elseif ($order === 'desc') {
-                rsort($array, SORT_NUMERIC);
-            }
+        function sort_2way($array, $order){  // sort_2way関数
+        if ($order === true) { // もしorderがtrueなら
+          echo '昇順にソートします。<br>' ;// 昇順にソートします。を表示。
+          sort($array); // 引数：array(配列)
+        }
+        elseif($order === false) { // trueではなく、falseだった場合
+          echo '降順にソートします。<br>';
+          rsort($array); // 引数：array(配列)
         }
 
-        // 昇順用にコピー
-        $asc_nums = $nums;
-        sort_2way($asc_nums, 'asc');
+        // 配列や連想配列に対して繰り返し処理を行いたい場合のやつ
+        foreach ($array as $num) { // 要素を取り出す配列名 as 取り出した要素を代入する変数名
+          echo $num .'<br>';
+        }
+        }
+        sort_2way($nums, true);
+        sort_2way($nums, false);
 
-        echo '昇順にソートします。<br>';
-        echo implode('<br>', $asc_nums);
-
-        echo '<br><br>';
-
-        // 降順用にコピー
-        $desc_nums = $nums;
-        sort_2way($desc_nums, 'desc');
-
-        echo '降順にソートします。<br>';
-        echo implode('<br>', $desc_nums);
-        ?>
-    </p>
-</body>
-
-</html>
+        
